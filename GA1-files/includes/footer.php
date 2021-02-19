01438edfbb0ac022a03d113f808c1b4f1b27e155
ibhatia <is998912@dal.ca>
2021-02-19 21:40:44 +0530

    Initial front end UI for the quizzes and the homepage

---
+++ GA1-files/includes/footer.php
@@ -0,0 +1,12 @@
+<!-- contributed by:
+    Name: Ishan Bhatia
+    Banner Number: B00835259
+    Implemented the front end aspects of the game -->
+
+    
+<footer>
+        <p> &copy; copyright</p>
+    </footer>
+    
+</body>
+</html>
---
+++ GA1-files/includes/header.php
@@ -0,0 +1,25 @@
+<!-- contributed by:
+    Name: Ishan Bhatia
+    Banner Number: B00835259
+    Implemented the front end aspects of the game -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Quiz</title>
+
+    <link rel="stylesheet" href="css/main.css" type='text/css'>
+
+</head>
+<body>
+
+    <header>
+        <a href= "index.php" id="home">HOME</a>
+        <a href= "index.php" id="quiz">TRIVIA QUIZ</a>
+        <a href= "#" id="instructions">INSTRUCTIONS</a>
+    </header>
