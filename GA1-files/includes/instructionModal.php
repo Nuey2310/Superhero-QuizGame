fc795f9f6d11e8b00bcbf3fadc7c1fcc64b2945d
dhairy <dh715291@dal.ca>
2021-02-27 19:24:14 +0530

    Added citations for instruction modal

--- GA1-files/includes/instructionModal.php
+++ GA1-files/includes/instructionModal.php
@@ -1,4 +1,12 @@
-
+<!-- contributed by:
+    Name: Dhairy Raval
+    Banner Number: B00845519
+    - Implemented the instruction modal for the game 
+    - Intext Citations:
+      URL: https://getbootstrap.com/docs/5.0/components/modal/
+      * Date accessed: 23 Jan 2021
+      * Credits to The Bootstrap team(https://getbootstrap.com/docs/5.0/about/team/) and contributors(https://github.com/twbs/bootstrap/graphs/contributors)
+    -->
 <!-- Modal -->
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
